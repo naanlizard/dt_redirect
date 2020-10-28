20e9011c981a0e17d127260e6c87361336ac3b71
jenbanim <jenbanim@gmail.com>
2020-10-28 10:40:28 +0000

    Make code not horribly bad and stupid
    Fix really bad and dumb issues with code that I really should've
    realized was a bad idea

--- dt_redirect.py
+++ dt_redirect.py
@@ -3,30 +3,37 @@ import os
 
 import praw
 
+
 class Redirect(BaseHTTPRequestHandler):
-    def __init__(self, *args):
-        self.reddit = praw.Reddit(
-            client_id = os.environ['client_id'],
-            client_secret = os.environ['client_secret'],
-            refresh_token = os.environ['refresh_token'],
-            user_agent = 'linux:dt_redirect:v1.0 (by /u/jenbanim)'
-        )
-        self.neoliberal = self.reddit.subreddit('neoliberal')
-        super().__init__(*args)
-
-    def _find_dt(self):
-        url = 'https://www.reddit.com/r/neoliberal'
-        for submission in self.neoliberal.search('Discussion Thread', sort='new'):
-            if submission.author == 'jobautomator':
-                url = submission.url
-                break
-        if self.path == '/dt/old':
-            url = url.replace('www', 'old')
-        return(url)
-
     def do_GET(self):
         self.send_response(302)
-        self.send_header('Location', self._find_dt())
+        self.send_header('Location', find_dt(self.path))
         self.end_headers()
 
-HTTPServer(("", 8080), Redirect).serve_forever()
+    def log_message(self, *_):
+        print(
+            f'{self.headers["X-Real-IP"]}:{self.command}:{self.path}'
+            f':{self.headers["User-Agent"]}'
+        )
+
+
+def find_dt(path = ''):
+    url = 'https://www.reddit.com/r/neoliberal'
+    for submission in neoliberal.search('Discussion Thread', sort='new'):
+        if submission.author == 'jobautomator':
+            url = submission.url
+            break
+    if path.strip('/') == 'dt/old':
+        url = url.replace('www', 'old')
+    return(url)
+
+
+if __name__ == "__main__":
+    reddit = praw.Reddit(
+        client_id = os.environ['client_id'],
+        client_secret = os.environ['client_secret'],
+        refresh_token = os.environ['refresh_token'],
+        user_agent = 'linux:dt_redirect:v1.1 (by /u/jenbanim)'
+    )
+    neoliberal = reddit.subreddit('neoliberal')
+    HTTPServer(("", 8080), Redirect).serve_forever()
